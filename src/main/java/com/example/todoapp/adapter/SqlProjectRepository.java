7da9d120d3d430eae3680783671a25070edc5784
Mateusz Lubowiecki <matlubowiecki@gmail.com>
2023-07-03 12:18:56 +0200

    Update SqlProjectRepository Update SqlTaskGroupRepository

--- src/main/java/com/example/todoapp/adapter/SqlProjectRepository.java
+++ src/main/java/com/example/todoapp/adapter/SqlProjectRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Repository
 public interface SqlProjectRepository extends ProjectRepository, JpaRepository<Project, Integer> {
     @Override
-    @Query("from Project p join fetch p.steps")
+    @Query("select distinct p from Project p join fetch p.steps")
     List<Project> findAll();
 
 
